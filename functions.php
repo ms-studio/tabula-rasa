b50e9884c6bbbff91dd9c5fd393f23e21563afe8
Manuel Schmalstieg <git.code@ms-studio.net>
2012-08-14 10:34:58 +0200

    custom login page link

--- functions.php
+++ functions.php
@@ -112,6 +112,19 @@ echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_dire
 }
 add_action('login_head', 'custom_login');
 
+// src: http://codex.wordpress.org/Customizing_the_Login_Form
+
+// change the link values so the logo links to your WordPress site
+function my_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'Your Site Name and Info';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
 
 /* admin interface
 ******************************/

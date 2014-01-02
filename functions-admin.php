35358e67048dbc683d68a6ee66b1548c4bbed467
Manuel Schmalstieg <git.code@ms-studio.net>
2014-01-02 23:14:05 +0100

    adapt to new dashboard boxes in WP 3.8.

--- functions-admin.php
+++ functions-admin.php
@@ -77,13 +77,11 @@ function tabula_remove_dashboard_widgets()
 	global $wp_meta_boxes;
 	
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+	
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
 		
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 	// RSS feeds:
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 	
 }
 add_action('wp_dashboard_setup', 'tabula_remove_dashboard_widgets' );

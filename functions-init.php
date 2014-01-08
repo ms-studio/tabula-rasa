80d341e94dc87c49892ca57d3d1ef971100f1c17
Manuel Schmalstieg <git.code@ms-studio.net>
2014-01-08 16:37:55 +0100

    move versioning function to init

--- functions-init.php
+++ functions-init.php
@@ -9,8 +9,39 @@
  * 5. 
 **************/
 
+
+/* CSS/JS Versioning
+*********************/
+
+// Custom Functions for CSS/Javascript Versioning
+$GLOBALS["TEMPLATE_URL"]          = get_bloginfo( 'template_url' ) . "/";
+$GLOBALS["TEMPLATE_RELATIVE_URL"] = wp_make_link_relative( $GLOBALS["TEMPLATE_URL"] );
+
+// Add ?v=[last modified time] to style sheets
+function versioned_stylesheet( $relative_url, $add_attributes = "" ) {
+	echo '<link rel="stylesheet" href="' . versioned_resource( $relative_url ) . '" ' . $add_attributes . '>' . "\n";
+}
+
+// Add ?v=[last modified time] to javascripts
+function versioned_javascript( $relative_url, $add_attributes = "" ) {
+	echo '<script src="' . versioned_resource( $relative_url ) . '" ' . $add_attributes . '></script>' . "\n";
+}
+
+// Add ?v=[last modified time] to a file url
+function versioned_resource( $relative_url ) {
+	$file         = $_SERVER["DOCUMENT_ROOT"] . $relative_url;
+	$file_version = "";
+
+	if ( file_exists( $file ) ) {
+		$file_version = "?v=" . filemtime( $file );
+	}
+
+	return $relative_url . $file_version;
+}
+
+
 // Unregister jQuery
-// (we call it in the footer)
+// (then re-register it in the footer)
 // ****************************
 
 function custom_register_styles() {

34dd81f32ac65255d5dfa9c1e3d606cf138b2540
Manuel Schmalstieg <git.code@ms-studio.net>
2014-03-10 23:28:50 +0100

    customize allowed file types.

--- functions-init.php
+++ functions-init.php
@@ -183,6 +183,29 @@ register_sidebar( array(
 ) );
 
 
+/* Allowed FileTypes
+ ********************
+ * method based on 
+ * http://howto.blbosti.com/?p=329
+ * List of defaults: https://core.trac.wordpress.org/browser/tags/3.8.1/src/wp-includes/functions.php#L1948
+*/
+add_filter('upload_mimes', 'custom_upload_mimes');
+function custom_upload_mimes ( $existing_mimes=array() ) {
+
+		// add an extension to the array
+		$existing_mimes['svg'] = 'image/svg+xml';
+
+		// remove existing file types
+		unset( $existing_mimes['bmp'] );
+		unset( $existing_mimes['tif|tiff'] );
+
+		// and return the new full result
+		return $existing_mimes;
+}
+  
+
+
+
 /*
  * Global Date Variables
 */

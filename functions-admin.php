143dfcb55b7c64db74c994ea8d61c398531b79f4
Manuel Schmalstieg <git.code@ms-studio.net>
2014-04-29 11:33:20 +0200

    function to remove "right now" block from dashboard, replace it with custom information.

--- functions-admin.php
+++ functions-admin.php
@@ -121,7 +121,9 @@ add_action( 'add_meta_boxes', 'remove_edit_fields' );
 function tabula_remove_dashboard_widgets() {
 	// Globalize the metaboxes array, this holds all the widgets for wp-admin
 	global $wp_meta_boxes;
-
+	
+	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+	
 	unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press'] );
 
 	unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity'] );
